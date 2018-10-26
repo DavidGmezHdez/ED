06c44ac685736310d283070097a6e94575d5ddc2
DavidGmezHdez <davidgmezhdez@gmail.com>
2018-10-26 12:11:21 +0200

    Acabar diccionario y make

--- Practica2/vector_dinamico/vector_dinamico/doc/html/annotated_dup.js
+++
@@ -1,4 +0,0 @@
-var annotated_dup =
-[
-    [ "Vector_Dinamico", "classVector__Dinamico.html", "classVector__Dinamico" ]
-];
--- Practica2/vector_dinamico/vector_dinamico/doc/html/dir_d44c64559bbebec7f509842c48db8b23.js
+++
@@ -1,6 +0,0 @@
-var dir_d44c64559bbebec7f509842c48db8b23 =
-[
-    [ "vector_dinamico.h", "vector__dinamico_8h.html", [
-      [ "Vector_Dinamico", "classVector__Dinamico.html", "classVector__Dinamico" ]
-    ] ]
-];
